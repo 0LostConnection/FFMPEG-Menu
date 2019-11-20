15b946bbacb1d9c8d304f015abdaaad8ea9911d0
NioiOtaku <40704593+NioiOtaku@users.noreply.github.com>
2019-11-20 10:14:01 -0300

    FFMPEG Menu V1.2
    ***Change Notes***
    
    ***-*** Type ```return``` on conversion menu for go to main menu
    ***-*** Unnecessary commands removed
    ***-*** Function explanation added

--- menu.bat
+++ menu.bat
@@ -1,10 +1,12 @@
+::Before of everything, sorry for my english, feel free to correct me :D
+
 echo off
-goto main
-set f%fmpeg%=ffmpeg.exe
+::Main Menu
 :main
 title Menu
 cls
 mode 30,13
+::Cecho = Color Changing Program
 cecho {0e}
 echo             FFMPEG
 echo           Select One
@@ -12,7 +14,7 @@ cecho {0f}
 echo.
 echo.
 echo.
-
+::cmdMenuSel = Program For Creation Of Selectable Options
 cmdMenuSel f870 "         -= Mp4-Mp3 =-" "         -= Mp4-Avi =-" "         -= Mp4-Gif =-" "         -= Mp3-Mp4 =-" "         -= Command =-" "         -=  Close  =-" " "
 if %ERRORLEVEL% == 1 goto Mp4-Mp3
 
@@ -26,18 +28,24 @@ if %ERRORLEVEL% == 5 goto code
 
 if %ERRORLEVEL% == 6 exit
 
+::Secret Option To Refresh The Menu
 if %ERRORLEVEL% == 7 goto main
 
-goto main
- 
+::Conversion Menu
 :Mp4-Mp3
 title Mp4-Mp3
 cd input
 cls
 cecho {0e}
+
 set /p input=Mp4-Input-File: 
+::Return Action
+if %input% == return goto gomain
 echo.
 set /p output=Mp3-Output-File: 
+::Return Action
+if %output% == return goto gomain
+::Start Of Conversion
 cd input
 color 0f
 mode 95,25
@@ -52,8 +60,13 @@ cd input
 cls
 cecho {0e}
 set /p input=Mp4-Input-File: 
+::Return Action
+if %input% == return goto gomain
 echo.
 set /p output=Avi-Output-File: 
+::Return Action
+if %output% == return goto gomain
+::Start Of Conversion
 cd input
 color 0f
 mode 95,25
@@ -68,8 +81,13 @@ cd input
 cls
 cecho {0e}
 set /p input=Mp4-Input-File: 
+::Return Action
+if %input% == return goto gomain
 echo.
 set /p output=Gif-Output-File: 
+::Return Action
+if %output% == return goto gomain
+::Start Of Conversion
 cd input
 color 0f
 mode 95,25
@@ -84,10 +102,17 @@ cd input
 cls
 cecho {0e}
 set /p input=Mp3-Input-File: 
+::Return Action
+if %input% == return goto gomain
 echo.
 set /p img=Img-Input-File: 
+::Return Action
+if %img% == return goto gomain
 echo.
 set /p output=Mp4-Output-File: 
+::Return Action
+if %output% == return goto gomain
+::Start Of Conversion
 cd input
 color 0f
 mode 95,25
@@ -96,9 +121,15 @@ move %output%.mp4 ..\output
 cd..
 goto main
 
+::Start Another CMD
 :code
 title Command
 start cmd
 goto main
 
-::ffmpeg -i %img% -i %input%.mp3 -acodec copy %output%.mp4
+::"Go To Main" Action
+:gomain
+cd..
+goto main
+
+::Thanks for looking my code! I hope you liked it!
